83b4c926a59ddfcddcd9a5cabd06af133a66b73b
Ian Muldoon <ian.muldoon.465@my.csun.edu>
2022-03-21 12:14:50 -0700

    Finished Login

---
+++ src/main/java/com/example/loginsystem/DatabaseConnection.java
@@ -0,0 +1,25 @@
+package com.example.loginsystem;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+    public Connection databaseLink;
+
+    public Connection getConnection(){
+        String databaseName = "userdatabase";
+        String databaseUser = "comp440";
+        String databasePassword = "pass1234";
+
+        String url = "jdbc:mysql://localhost/" + databaseName;
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            databaseLink = DriverManager.getConnection(url, databaseUser, databasePassword);
+        }catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return databaseLink;
+    }
+}
--- src/main/java/com/example/loginsystem/LoginPageController.java
+++ src/main/java/com/example/loginsystem/LoginPageController.java
@@ -2,12 +2,27 @@ package com.example.loginsystem;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 import java.io.IOException;
+import java.sql.*;
 
 public class LoginPageController {
     LoginPage login = new LoginPage();
+    @FXML
+    Button LoginButton;
+    @FXML
+    Button RegisterButton;
+    @FXML
+    TextField UsernameTextField;
+    @FXML
+    TextField PassTextField;
+    String passwordEntry;
+    String usernameEntry;
+    PreparedStatement prepareStatement;
+    ResultSet loginResult;
 
     public void handleInitDatabase(ActionEvent actionEvent) {
         System.out.println("Initialize Database!!");
@@ -20,6 +35,47 @@ public class LoginPageController {
     }
 
     public void handleLogin(ActionEvent actionEvent) {
-        System.out.println("Log in");
+
+        //get login fields
+        passwordEntry = PassTextField.getText();
+        usernameEntry = UsernameTextField.getText();
+
+        //set up connections
+        DatabaseConnection connectU = new DatabaseConnection();
+        Connection connectUser = connectU.getConnection();
+
+        DatabaseConnection connectP = new DatabaseConnection();
+        Connection connectPass = connectP.getConnection();
+
+        //Username query
+        String loginQuery = "SELECT username, password FROM users where username= ?";
+
+
+        try {
+            //create prepared statements
+            prepareStatement = connectUser.prepareStatement(loginQuery);
+
+            //insert parameters
+            prepareStatement.setString(1, usernameEntry);
+
+            //Execute Prepared Statement
+            loginResult = prepareStatement.executeQuery();
+
+            //get results
+            if(!loginResult.isBeforeFirst()){
+                System.out.println("Try Again");
+            }else {
+                while (loginResult.next()) {
+                    if (usernameEntry.equals(loginResult.getString("username")) && passwordEntry.equals(loginResult.getString("password"))) {
+                        System.out.println("Login successful!");
+                    } else {
+                        System.out.println("Try Again");
+                    }
+                }
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
