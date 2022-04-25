05dcd168737fc441903c7f1a7a9017ac561408b3
Ian Muldoon <ian.muldoon.465@my.csun.edu>
2022-04-24 17:02:30 -0700

    Finished part 3

--- src/main/java/com/example/loginsystem/QueryPageController.java
+++ src/main/java/com/example/loginsystem/QueryPageController.java
@@ -14,10 +14,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.ResourceBundle;
-import java.util.Set;
+import java.util.*;
 
 
 public class QueryPageController {
@@ -56,11 +53,13 @@ public class QueryPageController {
 
         return preparedStatement;
     }
+    //This
     public String queryOne() throws SQLException {
-        PreparedStatement preparedStatement;
         ResultSet resultSet;
+        PreparedStatement preparedStatement;
+        ArrayList<String> input = new ArrayList<>();
         ArrayList<String> result = new ArrayList<>();
-        Set<String> stringSet = new HashSet<>();
+        HashMap<Integer, String> stringMap = new HashMap<Integer, String>();
 
         String X = xTextField.getText();
         String Y = yTextField.getText();
@@ -68,27 +67,31 @@ public class QueryPageController {
             System.out.println("You must enter in values for X and Y");
         } else {
 
-            preparedStatement = queryPreparedStatement("SELECT username FROM users WHERE id IN (SELECT usersID FROM blog WHERE id IN (SELECT blogID FROM tags WHERE Title = ? ));");
+            preparedStatement = queryPreparedStatement("SELECT users.username, blog.id FROM users, blog WHERE blog.usersID = users.id AND blog.id IN (SELECT blogID FROM tags WHERE Title = ?)");
             preparedStatement.setString(1, X);
             resultSet = preparedStatement.executeQuery();
             while (resultSet.next()) {
-                String queryResult = resultSet.getString("username");
-                stringSet.add(queryResult);
+                String queryResultUsername = resultSet.getString("username");
+                int queryResultID = Integer.parseInt((resultSet.getString("id")));
+                stringMap.put(queryResultID, queryResultUsername);
             }
-            preparedStatement = queryPreparedStatement("SELECT username FROM users WHERE id IN (SELECT usersID FROM blog WHERE id IN (SELECT blogID FROM tags WHERE Title = ? ));");
-            preparedStatement.setString(1,Y);
+            preparedStatement = queryPreparedStatement("SELECT users.username, blog.id FROM users, blog WHERE blog.usersID = users.id AND blog.id IN (SELECT blogID FROM tags WHERE Title = ?);");
+            preparedStatement.setString(1, Y);
             resultSet = preparedStatement.executeQuery();
             while (resultSet.next()) {
-                String queryResult = resultSet.getString("username");
-                if (stringSet.add(queryResult) == false) {
-                    result.add(queryResult);
-                }
+                String queryResultUsername = resultSet.getString("username");
+                int queryResultID = Integer.parseInt((resultSet.getString("id")));
 
+                for (Map.Entry<Integer, String> entry : stringMap.entrySet()) {
+                    if (entry.getValue().equals(queryResultUsername) && entry.getKey() != queryResultID) {
+                        result.add(queryResultUsername);
+                    }
+                }
             }
-            String listString = String.join(", ", result);
-            answerText.setText(listString);
-            return listString;
-        }
+                String listString = String.join(", ", result);
+                answerText.setText(listString);
+                return listString;
+            }
         return "";
     }
     public String queryTwo() throws SQLException {
@@ -118,7 +121,125 @@ public class QueryPageController {
         return "";
     }
 
+    public String queryThree() throws SQLException {
+        PreparedStatement preparedStatement;
+        ResultSet resultSet;
+        ArrayList<String> result = new ArrayList<>();
 
+        preparedStatement = queryPreparedStatement("WITH CTE_BlogCount AS ( SELECT usersID, count(usersID) AS blogPosts FROM blog WHERE createdON BETWEEN '2022-04-22 00:00:00' AND '2022-04-22 23:59:59' group by usersID order by blogPosts DESC ) , CTE_MaxBlogs AS  ( SELECT MAX(BlogPosts) AS MAX FROM CTE_BlogCount ) SELECT U.Username, Bc.BlogPosts FROM CTE_BlogCount BC , CTE_MaxBlogs MB , Users U WHERE BC.BlogPosts = MB.MAX AND U.ID = BC.Usersid;");
+        resultSet = preparedStatement.executeQuery();
+        while(resultSet.next()) {
+            String user = resultSet.getString("username");
+            result.add(user);
+        }
+        String listString = String.join(", ", result);
+        answerText.setText(listString);
+        return listString;
+        }
+
+    public String queryFour() throws SQLException {
+        PreparedStatement preparedStatement;
+        ResultSet resultSet;
+        String X = xTextField.getText();
+        String Y = yTextField.getText();
+        ArrayList<String> result = new ArrayList<>();
+
+        if (X.equals("") || Y.equals("")) {
+            System.out.println("Please enter a username for X and Y");
+        } else {
+            preparedStatement = queryPreparedStatement("Select username From users Where id IN (select FollowedUserid From follow Where FollowingUserid = (Select id FROM users WHERE username = ?) AND id IN (select FollowedUserid From follow Where FollowingUserid = (Select id FROM users WHERE username = ?)));");
+            preparedStatement.setString(1, X);
+            preparedStatement.setString(2, Y);
+            resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                result.add(resultSet.getString("username"));
+            }
+            String listString = String.join(", ", result);
+            answerText.setText(listString);
+            return listString;
+        }
+        return "";
+    }
+    public String queryFive() throws SQLException{
+        PreparedStatement preparedStatement;
+        ResultSet resultSet;
+        ArrayList<String> result = new ArrayList<>();
+
+        preparedStatement = queryPreparedStatement("SELECT t1.usersID, t2.usersID FROM hobbies AS t1 JOIN hobbies AS t2 ON t1.usersID < t2.usersID WHERE 2 = ALL (SELECT count(*) FROM hobbies WHERE (t1.hiking = 1 OR t2.hiking = 1) AND (t1.swimming = 1 OR t2.swimming = 1) AND (t1.calligraphy = 1 OR t2.calligraphy = 1) AND (t1.bowling OR t2.bowling = 1) AND (t1.movie = 1 OR t2.movie = 1) AND (t1.cooking = 1 OR t2.cooking = 1) AND (t1.dancing = 1 OR t2.dancing = 1) group by id);");
+        resultSet = preparedStatement.executeQuery();
+        while (resultSet.next()) {
+            String userIDOne = resultSet.getString(1);
+            String userIDTwo = resultSet.getString(2);
+            result.add("(" + userIDOne + "," + userIDTwo + "), ");
+        }
+        String listString = String.join(" ", result);
+        answerText.setText(listString);
+        return listString;
+    }
+
+    public String querySix() throws SQLException{
+        PreparedStatement preparedStatement;
+        ResultSet resultSet;
+        ArrayList<String> result = new ArrayList<>();
+
+        preparedStatement = queryPreparedStatement("Select id from users Where id Not IN (select usersID From blog);");
+        resultSet = preparedStatement.executeQuery();
+        while (resultSet.next()) {
+            String user = resultSet.getString(1);
+            result.add(user + ", ");
+        }
+        String listString = String.join(" ", result);
+        answerText.setText(listString);
+        return listString;
+    }
+
+    public String querySeven() throws SQLException{
+        PreparedStatement preparedStatement;
+        ResultSet resultSet;
+        ArrayList<String> result = new ArrayList<>();
+
+        preparedStatement = queryPreparedStatement("Select username From users Where id Not IN (select usersID From comments);");
+        resultSet = preparedStatement.executeQuery();
+        while(resultSet.next()){
+            String user = resultSet.getString(1);
+            result.add(user + ", ");
+        }
+        String listString = String.join(" ", result);
+        answerText.setText(listString);
+        return listString;
+    }
+
+    public String queryEight() throws SQLException{
+        PreparedStatement preparedStatement;
+        ResultSet resultSet;
+        ArrayList<String> result = new ArrayList<>();
+
+        preparedStatement = queryPreparedStatement("SELECT username FROM users WHERE id IN (SELECT usersID FROM comments WHERE sentiment = 0 AND usersID NOT IN (SELECT usersID FROM comments WHERE Sentiment = 1));");
+        resultSet = preparedStatement.executeQuery();
+        while(resultSet.next()) {
+            String user = resultSet.getString(1);
+            result.add(user + ", ");
+        }
+        String listString = String.join(" ", result);
+        answerText.setText(listString);
+        return listString;
+    }
+
+    public String queryNine() throws SQLException{
+        PreparedStatement preparedStatement;
+        ResultSet resultSet;
+        ArrayList<String> result = new ArrayList<>();
+
+        preparedStatement = queryPreparedStatement("SELECT username FROM users WHERE id  IN (SELECT usersID FROM blog WHERE id IN (SELECT blogID FROM comments WHERE Sentiment != 0 ));");
+        resultSet = preparedStatement.executeQuery();
+        while(resultSet.next()) {
+            String user = resultSet.getString(1);
+            result.add(user + ", ");
+        }
+        String listString = String.join(" ", result);
+        answerText.setText(listString);
+        return listString;
+    }
 
 
 
@@ -132,13 +253,36 @@ public class QueryPageController {
 
 
     public void handleExecuteButton(ActionEvent actionEvent) throws SQLException {
-        switch(queryNumber){
-            case "1": queryOne();
-                break;
-            case "2": queryTwo();
-                break;
-
+        if (queryNumber != null) {
+            switch (queryNumber) {
+                case "1":
+                    queryOne();
+                    break;
+                case "2":
+                    queryTwo();
+                    break;
+                case "3":
+                    queryThree();
+                    break;
+                case "4":
+                    queryFour();
+                    break;
+                case "5":
+                    queryFive();
+                    break;
+                case "6":
+                    querySix();
+                    break;
+                case "7":
+                    querySeven();
+                    break;
+                case "8":
+                    queryEight();
+                    break;
+                case "9":
+                    queryNine();
+                    break;
+            }
         }
-
     }
 }
